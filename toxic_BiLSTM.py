2ad0bc672c093c16f6b73c2e20fc746a97a97e54
avinash3001 <avinashrao3001@gmail.com>
2022-08-26 16:46:54 +0100

    BLSTM small changes

--- toxic_BiLSTM.py
+++ toxic_BiLSTM.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import os
 
 from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
+from keras_preprocessing.sequence import pad_sequences
 from keras.layers import Dense, Input, Embedding
 from keras.layers import Bidirectional, LSTM, GlobalMaxPool1D
 from keras.models import Model
@@ -18,7 +18,7 @@ MAX_VOC = 20000
 SEQ_LENGTH = 200
 
 word2Vec = {}
-with open(os.path.join(f'../generic_data/glove.6B/glove.6B.{EMBEDDING_DIM}d.txt')) as f:
+with open(os.path.join(f'../generic_data/glove.6B/glove.6B.{EMBEDDING_DIM}d.txt'), encoding='utf-8') as f:
   for line in f:
     row = line.split()
     word2Vec[row[0]] = np.asarray(row[1:], dtype='float32')
@@ -58,7 +58,7 @@ input_length=200)
 
 input = Input(shape=(200,))
 x = embedded_lyr(input)
-x = Bidirectional(LSTM(15, activation='relu', return_sequences=True))(x)
+x = Bidirectional(LSTM(15, activation='tanh', return_sequences=True))(x)
 x = GlobalMaxPool1D()(x)
 x = Dense(128, activation='relu')(x)
 output = Dense(len(tar), activation= 'sigmoid')(x)
@@ -79,14 +79,14 @@ res = model.fit(
   validation_split=0.2
 )
 
-plt.plot(res.hisory['loss'], label= 'loss')
-plt.plot(res.hisory['val_loss'], label='Val_loss')
+plt.plot(res.history['loss'], label= 'loss')
+plt.plot(res.history['val_loss'], label='Val_loss')
 plt.legend()
 plt.show()
 
 
-plt.plot(res.hisory['accuracy'], label= 'acc')
-plt.plot(res.hisory['val_accuracy'], label='Val_acc')
+plt.plot(res.history['accuracy'], label= 'acc')
+plt.plot(res.history['val_accuracy'], label='Val_acc')
 plt.legend()
 plt.show()
 
